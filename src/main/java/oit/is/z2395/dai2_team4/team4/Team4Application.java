28fdcaabd754f9e422231d02bc9860e0c9b059cc
Yura-Mp <e1b22079@oit.ac.jp>
2024-10-01 14:26:58 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/z2395/dai2_team4/team4/Team4Application.java
@@ -0,0 +1,13 @@
+package oit.is.z2395.dai2_team4.team4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team4Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/z2395/dai2_team4/team4/Team4ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.z2395.dai2_team4.team4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
